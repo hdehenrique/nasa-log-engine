e6f4aa9b7e57ecffcbbc5a0191783fce9f48ccab
hdehenrique <concertti@outlook.com>
2025-05-16 16:54:47 -0300

    add project 2020

---
+++ DesafioSemantix/processLogs/target/classes/br/handlers/SparkHandler.scala
@@ -0,0 +1,11 @@
+package br.handlers
+import org.apache.spark.sql.{SQLContext, SparkSession}
+
+
+object SparkHandler {
+
+  def getSparkSession: SparkSession = {sparkSession }
+
+  val sparkSession: SparkSession = SparkSession.builder().appName("httpRequest").getOrCreate()
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# nasa-log-engine
-Análise de logs da NASA proposto pela Semantix, utilizando Apache Spark e Scala para processar e extrair insights de grandes volumes de dados.
